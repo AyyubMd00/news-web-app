f409a056f6516e8c07ed6a049ecb289d3410e746
AyyubMd00 <thahamdsaad@gmail.com>
2023-11-13 20:12:30 +0530

    Web scraping is developed using Beautiful Soup

--- Functions/function_app.py
+++ Functions/function_app.py
@@ -3,8 +3,8 @@ import azure.functions as func
 # import sys
 
 # sys.path.append("Web Scraping")
-from web_scraping.ie_get_city_news import get_city_news
-from web_scraping.th_get_latest_news import get_latest_news
+from web_scraping.ie_get_news import get_news as ie_get_news
+from web_scraping.th_get_news import get_news as th_get_news
 app = func.FunctionApp()
 
 @app.schedule(schedule="0 0,30 * * * *", arg_name="myTimer",
@@ -12,15 +12,14 @@ app = func.FunctionApp()
 def FetchNews(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
-    get_latest_news()
-    print('Function has run')
-    logging.info('Both functions executed.')
+    th_get_news()
+    logging.info('Python timer trigger function executed.')
 
-@app.timer_trigger(schedule="0 15,45 * * * *", arg_name="myTimer", run_on_startup=True,
+@app.timer_trigger(schedule="0 15,45 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def FetchNewsIE(myTimer: func.TimerRequest) -> None:
     
     if myTimer.past_due:
         logging.info('The timer is past due!')
-    get_city_news()
+    ie_get_news()
     logging.info('Python timer trigger function executed.')
