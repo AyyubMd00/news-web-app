d52b5e432f4b1ec05ed6b3c99339b950083a0df4
AyyubMd00 <thahamdsaad@gmail.com>
2023-11-17 22:57:38 +0530

    Functions are created and deployed

--- Functions/function_app.py
+++ Functions/function_app.py
@@ -2,40 +2,43 @@ import logging
 import azure.functions as func
 import requests
 
-# sys.path.append("Web Scraping")
-from web_scraping.ie_get_news import get_news as ie_get_news
 from web_scraping.th_get_news import get_news as th_get_news
+from web_scraping.ie_get_news import get_news as ie_get_news
 from utils.predict_category import predict_category
 app = func.FunctionApp()
-predict_category_function_url = 'https://func-news-app-process-news.azurewebsites.net/api/PredictCategory'
+predict_category_func_url = "https://func-news-app-process-news.azurewebsites.net/api/PredictCategory"
 
-@app.schedule(schedule="0 0,30 * * * *", arg_name="myTimer",
+@app.schedule(schedule="0 0,30 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
-def FetchNews(myTimer: func.TimerRequest) -> None:
+def FetchNewsTH(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
     th_get_news()
-    
-    requests.get(predict_category_function_url)
+    response = requests.get(predict_category_func_url)
+    if response.status_code == 200:
+        logging.info('PredictCategory triggered successfully from FetchNewsTH.')
+    else:
+        logging.info('Predict Category Function trigger failed.')
     logging.info('Python timer trigger function executed.')
 
 @app.timer_trigger(schedule="0 15,45 * * * *", arg_name="myTimer", run_on_startup=False,
               use_monitor=False) 
 def FetchNewsIE(myTimer: func.TimerRequest) -> None:
-    
     if myTimer.past_due:
         logging.info('The timer is past due!')
     ie_get_news()
-    requests.get(predict_category_function_url)
+    response = requests.get(predict_category_func_url)
+    if response.status_code == 200:
+        logging.info('PredictCategory triggered successfully from FetchNewsIE.')
+    else:
+        logging.info('Predict Category Function trigger failed.')
     logging.info('Python timer trigger function executed.')
 
 @app.route(route="PredictCategory", auth_level=func.AuthLevel.ANONYMOUS)
 def PredictCategory(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    
     predict_category()
-    
     return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200
+        "This HTTP triggered function executed successfully.",
+        status_code=200
     )
--- Functions/test.py
+++ Functions/test.py
@@ -9,12 +9,14 @@ from datetime import datetime
 
 # from web_scraping.th_get_latest_news import get_latest_news
 from utils.get_category_bert import get_category
+from utils.predict_category import predict_category
 
 start_time = datetime.now()
 print(start_time)
-title = "Delhi HC to consider framing policy to deal with property of children who have lost parents"
-description = "The high court will hear the matter next on December 6."
+title = "Man thrashed and sexually assaulted in Delhi; police arrest 2"
+description = "The incident took place on Diwali night when the complainant and the family of the accused were celebrating the festival."
 print(get_category(title, description))
+predict_category()
 # get_news()
 # get_city_news()
 # print(get_story('https://www.thehindu.com/sport/cricket/captaincy-has-not-been-a-burden-for-me-babar-azam/article67522001.ece'))
