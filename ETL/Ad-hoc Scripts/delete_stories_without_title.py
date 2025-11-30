9c926b698827ea2f631f88b80ccba5b44ee93d4e
unknown <thahamdsaad@gmail.com>
2025-11-30 19:50:21 +0530

    Repo Cleanup

--- ETL/Ad-hoc Scripts/delete_stories_without_title.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-sys.path.append('Functions\\utils')
-from db_utils import delete_stories
-
-query = {'title': ""}
-delete_stories(query)
--- ETL/Ad-hoc Scripts/migrate_news_records_to_arch.py
+++
@@ -1,1 +0,0 @@
-from db_utils import query_stories, update_category_in_db
